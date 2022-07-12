e3f6ebaccb19c16cb031dac14a34c353ecb6e091
Claire Thorington <claire.thorington@gmail.com>
2022-07-12 15:12:47 -0700

    create initial files

---
+++ Triangle.Tests/ModelTests/TriangleTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+
+namespace Triangle.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  {
+    // Test methods go here
+  }
+}
